dc8b675a2a1864bbb0074417778b659f324b218c
ecarplug <ecarp.sk.park@ecarplug.com>
2021-07-19 18:06:10 +0900

    first Commit

---
+++ collectionTest1/Pods/Target Support Files/Pods-collectionTest1/Pods-collectionTest1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_collectionTest1 : NSObject
+@end
+@implementation PodsDummy_Pods_collectionTest1
+@end
---
+++ collectionTest1/collectionTest1/README.md
@@ -0,0 +1,2 @@
+# SwipeTableViewTest
+# SwipeTableViewTest
